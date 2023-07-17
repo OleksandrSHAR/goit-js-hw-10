8370381c74fdb640cb27758944cc4e7caa89880b
OleksandrSHAR <125485600+OleksandrSHAR@users.noreply.github.com>
2023-07-17 18:53:48 +0300

    goit-js-hw-10

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 axios.defaults.headers.common["x-api-key"] = "live_E5iLB6lkwpo1ELiOf9rMpBJuuGUlUje6JIpXeVXjQpEBRXCoPT9J3vZs9JTCIWhE";
 
-import fetchBreeds from './cat-api'
-import fetchBreedsBre from './cat-api'
-import BASE_URL from'./cat-api'
+
  
 const catInf = document.querySelector('.cat-info')
 const infError = document.querySelector('.error')
@@ -11,22 +9,38 @@ const infLoad = document.querySelector('.loader')
 const breedSelect = document.querySelector('.breed-select')
 const body = document.querySelector('body')
 
+// axios.get('/breeds/', {
+//     params: {
+//         name: '',
+//         id:''
+//     }
+//   })
+//   .then(function (response) {
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   })
+//   .finally(function () {
+//     // always executed
+//   });
 
 
 body.style.backgroundImage =
     infError.style.visibility = 'hidden';
     infLoad.style.visibility = 'hidden';
 
-breedSelect.addEventListener('imput',imputBreed)
-function imputBreed(e) {
-  
+breedSelect.addEventListener('imput',catSelect)
+
+function catSelect(items) {
+    const breedColect = items.map(({ id, name }) => { return `<option value="${id}">${name}</option>` }).join('');
+    breedSelect.innerHTML = breedColect;
 }
 
-
 function etchBreedsBre(items) {
-    const breedList = items.map(({ url, id, wikipedia_url, temperament, breeds }) => {
+    const breedList = items.map(({ url, name, wikipedia_url, temperament, breeds }) => {
         return `
-        <img src="${url}" alt="${id}">
+        <img src="${url}" alt="${name}">
         <h1>${breeds}</h1>
         <p>${wikipedia_url}</p>
         <p>${temperament}</p>
