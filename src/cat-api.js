7c520c4701cee32721e7a2ca755d5b9019f35a92
OleksandrSHAR <125485600+OleksandrSHAR@users.noreply.github.com>
2023-07-18 16:26:01 +0300

    goit-js-hw-10

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 axios.defaults.headers.common["x-api-key"] = "live_E5iLB6lkwpo1ELiOf9rMpBJuuGUlUje6JIpXeVXjQpEBRXCoPT9J3vZs9JTCIWhE";
-axios.defaults.BASE_URL = 'https://api.thecatapi.com/v1/'
+axios.defaults.baseURL = 'https://api.thecatapi.com/v1/'
 
 export function fetchBreeds() {
   return axios.get('/breeds/').then(resp => {
@@ -12,7 +12,7 @@ export function fetchBreeds() {
 }
 
 
-export function fetchBreedsBre(breedId) {
+export function fetchBreedsCat(breedId) {
     return axios.get(`/images/search?breed_ids=${breedId}`).then(resp => {
         if (resp !== 200) {
           throw new Error(resp.status)
--- src/index.js
+++ src/index.js
@@ -1,51 +1,41 @@
 import axios from "axios";
 axios.defaults.headers.common["x-api-key"] = "live_E5iLB6lkwpo1ELiOf9rMpBJuuGUlUje6JIpXeVXjQpEBRXCoPT9J3vZs9JTCIWhE";
+import { fetchBreeds} from "./cat-api";
+import {fetchBreedsCat} from "./cat-api";
 
-
- 
+ axios . isCancel ( fetchBreeds )
 const catInf = document.querySelector('.cat-info')
 const infError = document.querySelector('.error')
 const infLoad = document.querySelector('.loader')
 const breedSelect = document.querySelector('.breed-select')
 const body = document.querySelector('body')
 
-// axios.get('/breeds/', {
-//     params: {
-//         name: '',
-//         id:''
-//     }
-//   })
-//   .then(function (response) {
-//     console.log(response);
-//   })
-//   .catch(function (error) {
-//     console.log(error);
-//   })
-//   .finally(function () {
-//     // always executed
-//   });
-
-
-body.style.backgroundImage =
-    infError.style.visibility = 'hidden';
-    infLoad.style.visibility = 'hidden';
-
-breedSelect.addEventListener('imput',catSelect)
-
+fetchBreeds().then(data => {
+    catSelect(data)
+})
 function catSelect(items) {
     const breedColect = items.map(({ id, name }) => { return `<option value="${id}">${name}</option>` }).join('');
     breedSelect.innerHTML = breedColect;
 }
+body.style.backgroundImage =
+    infError.style.visibility = 'hidden';
+    infLoad.style.visibility = 'hidden';
 
-function etchBreedsBre(items) {
-    const breedList = items.map(({ url, name, wikipedia_url, temperament, breeds }) => {
+breedSelect.addEventListener('chenge',etchBreedsBre)
+
+
+function etchBreedsBre(e) {
+    infLoad.removeAttribute('hidden')
+    const breedList = fetchBreedsCat(e.target.value).then(data => data.map(({ url, name, description, temperament, breeds }) => {
         return `
-        <img src="${url}" alt="${name}">
-        <h1>${breeds}</h1>
-        <p>${wikipedia_url}</p>
+        <img src="${url}" alt="cat" width="400"height="400">
+        <h1>${name}</h1>
+        <p>${description}</p>
         <p>${temperament}</p>
         `
-    }).join('');
+    })).join('');
+    
     catInf.innerHTML = breedList;
-    return breedList
+    return breedList;
+    
 }   
