8dc7832e6adcb30c4fcc289a41dfd6809ca3f0e3
OleksandrSHAR <125485600+OleksandrSHAR@users.noreply.github.com>
2023-07-18 17:00:48 +0300

    goit-js-hw-10

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-axios.defaults.headers.common["x-api-key"] = "live_E5iLB6lkwpo1ELiOf9rMpBJuuGUlUje6JIpXeVXjQpEBRXCoPT9J3vZs9JTCIWhE";
+axios.defaults.headers.common['x-api-key'] ="live_hAPwe9T2sAFqIdAaEVWLWnLgPvoQnZJvaq8wz7DiapKWYNWcPv75pqIDfZxNu5yC";
 axios.defaults.baseURL = 'https://api.thecatapi.com/v1/'
 
 export function fetchBreeds() {
